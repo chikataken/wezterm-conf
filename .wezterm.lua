e93e2fe0fa5a321311f040b796fc678fd18f3549
andrew ink <64388518+chikataken@users.noreply.github.com>
2025-07-02 02:30:59 -0700

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -12,23 +12,7 @@ config.font = wezterm.font 'JetBrains Mono'
 config.window_background_opacity = 0.95
 
 -- ssh
-config.default_prog = { '/bin/zsh', '-l' } :contentReference[oaicite:0]{index=0}
-
-config.keys = {
-
-  {
-    key    = 'n',
-    mods   = 'CMD',
-    action = wezterm.action.SpawnCommandInNewWindow {
-      args = { '/usr/bin/ssh', 'mbdtf' },
-    },
-  },
-
-  {
-    key    = 't',
-    mods   = 'CMD',
-    action = wezterm.action.SpawnTab 'CurrentPaneDomain',
-  },
-}
+config.default_prog = { '/bin/zsh', '-l' }
+config.default_gui_startup_args = { 'ssh', 'mbdtf'}
 
 return config
