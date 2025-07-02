7586a2b2ba7e7dc173262d968732e680b3289e3f
andrew ink <64388518+chikataken@users.noreply.github.com>
2025-07-02 02:26:45 -0700

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -12,16 +12,9 @@ config.font = wezterm.font 'JetBrains Mono'
 config.window_background_opacity = 0.9
 
 -- ssh
-config.default_prog = { "/bin/zsh", "-l" }
-local ssh_target = "mbdtf"
-config.launch_menu = {
-  {
-    label = "SSH → " .. ssh_target,
-    args  = { "/usr/bin/ssh", ssh_target },
-    cwd   = wezterm.home_dir,
-    domain = "CurrentPaneDomain",
-  },
-}
-config.default_gui_startup_args = { "LaunchMenuEntry", "SSH → "..ssh_target }
+config.default_prog = { '/bin/zsh', '-l' }
+local ssh_target = 'mbdtf'
+config.default_gui_startup_args = { 'ssh', ssh_target }
+
 
 return config
