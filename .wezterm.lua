bc39a245c1f8961bd69e1f2a495ffd3de6741915
andrew ink <64388518+chikataken@users.noreply.github.com>
2025-07-02 02:01:14 -0700

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,11 +7,11 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices.
 
 -- For example, changing the initial geometry for new windows:
-config.initial_cols = 200
-config.initial_rows = 200
+config.initial_cols = 100
+config.initial_rows = 100
 
 -- or, changing the font size and color scheme.
-config.font_size = 14
+config.font_size = 12
 config.color_scheme = 'AdventureTime'
 
 -- Finally, return the configuration to wezterm:
